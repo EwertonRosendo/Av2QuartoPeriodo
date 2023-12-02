90bb2245e45ce5b2afc37686f54a71e5386c51ee
Rosendo <ewerton.rosendo15@gmail.com>
2023-12-01 22:56:56 -0300

    form v.1

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LocalizacaoComponent } from './localizacao/localizacao.component';
 import { SobreComponent } from './sobre/sobre.component';
-import { MenuComponent } from './menu/menu.component';
+
 
 @NgModule({
   declarations: [
@@ -14,11 +16,14 @@ import { MenuComponent } from './menu/menu.component';
     HomeComponent,
     LocalizacaoComponent,
     SobreComponent,
-    MenuComponent
+    
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,25 +1,37 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import {Cliente} from "../clientes/shared/cliente"
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
-  constructor(){
+  formCliente: FormGroup = new FormGroup({});
 
-}
-senha: string = "";
-tamanho_senha: number = 0;
-username:string = "";
+  constructor(private formBuilder: FormBuilder) { }
 
-setTmhSenhaAndUser(tmh:number, username:string){
-  this.tamanho_senha = tmh;
-  this.username = username;
-}
+  ngOnInit() {
+    this.createForm(new Cliente());
+  }
 
+  createForm(cliente: Cliente) {
+    this.formCliente = this.formBuilder.group({
+      nome: [cliente.nome],
+      tipo: [cliente.tipo],
+      genero: [cliente.genero],
+      observacao: [cliente.observacao],
+      inativo: [cliente.inativo]
+    })
+  }
+  onSubmit() {
+    // aqui você pode implementar a logica para fazer seu formulário salvar
+    console.log(this.formCliente.value);
+  
+    // chamando a função createForm para limpar os campos na tela
+    this.createForm(new Cliente());
+  }
 
 
 }
