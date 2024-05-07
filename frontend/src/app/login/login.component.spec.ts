d0c2814af2c28d6b696e56f561259d7b9c7b0fcf
Diigs <117246328+Diigs@users.noreply.github.com>
2024-05-06 21:46:06 -0300

    verifica se o campo email está vazio

--- frontend/src/app/login/login.component.spec.ts
+++ frontend/src/app/login/login.component.spec.ts
@@ -1,39 +1,30 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing'; // Importe o HttpClientTestingModule
 import { RouterTestingModule } from '@angular/router/testing'; // Importe o RouterTestingModule
 import { LoginComponent } from './login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 describe('LoginComponent', () => {
-  let fixture: ComponentFixture<LoginComponent>;
-  let componentInstance: LoginComponent;  
+  let fixture: ComponentFixture<LoginComponent>; // Declara uma variável para manipular o componente durante os testes
+  let componentInstance: LoginComponent;  // Declara uma variável para acessar as propriedades e métodos do componente durante os testes
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, RouterTestingModule, ReactiveFormsModule], // Adicione o HttpClientTestingModule e o RouterTestingModule aos imports
-      declarations: [LoginComponent],
-    }).compileComponents();
-    fixture = TestBed.createComponent(LoginComponent); // Crie uma instância do LoginComponent
-    componentInstance = fixture.componentInstance;  // Atribua a instância do LoginComponent a componentInstance
-  }); 
-
-  it("it email or password is in required state", () => {
-    fixture.detectChanges(); // Detecte as mudanças no componente
-    fixture.whenStable().then(() =>{ // Aguarde até que o estado do componente seja estável
-      const emailElement: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#email'); // Encontre o elemento de entrada de e-mail no DOM
-      emailElement.value = 'lucas_pereira.99@hotmail.com'; // Defina o valor do campo de e-mail
-      emailElement.dispatchEvent(new Event('input')); // Simule um evento de entrada para refletir a mudança de valor do campo de e-mail
-
-      const passwordElement: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#password'); // Encontre o elemento de entrada de senha no DOM
-      passwordElement.value = '123456'; // Defina o valor do campo de senha
-      passwordElement.dispatchEvent(new Event('input')); // Simule um evento de entrada para refletir a mudança de valor do campo de senha
-
-      fixture.detectChanges(); // Detecte as mudanças no componente após as alterações
-      fixture.whenStable().then(() => { // Aguarde até que o estado do componente seja estável após as alterações
-        const btnElement: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#button'); // Encontre o elemento do botão no DOM
-        expect(btnElement.disabled).toBeFalsy(); // Verifique se o botão não está desativado
-        expect(componentInstance.formUser.get('email')?.value).toEqual('lucas_pereira.99@hotmail.com'); // Verifique se o valor do campo de e-mail no formulário é igual ao valor esperado
-      });
+  beforeEach(waitForAsync(() => { // Configura o ambiente de teste antes de executar cada teste
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule, ReactiveFormsModule], // Adicione módulos necessários para o teste
+      declarations: [LoginComponent], // Declare o componente a ser testado
+    }).compileComponents().then(() => {
+      fixture = TestBed.createComponent(LoginComponent); // Crie uma instância do LoginComponent
+      componentInstance = fixture.componentInstance;  // Atribua a instância do LoginComponent a componentInstance
     });
+  })); 
+
+  it("verifica se o campo do email está vazio", () => {
+    const emailElement: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#email');
+    //emailElement.value = '123'; // Definir o campo de e-mail como vazio
+    //emailElement.dispatchEvent(new Event('input')); // Emitir um evento de input
+  
+    fixture.detectChanges();   
+    expect(componentInstance.formUser.get('email')?.value).toEqual(''); // Verificar se o campo de e-mail está vazio
   });
+  
 });
